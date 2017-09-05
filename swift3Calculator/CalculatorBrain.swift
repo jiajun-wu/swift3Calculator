e6a075d5c1d398f0acec9e66c6664ccf013d9e78
Jiajun Wu <jiajunwu@Jiajuns-MacBook-Pro.local>
2017-09-05 12:19:23 -0700

    adding digits and CalculatorBrain.swift

---
+++ swift3Calculator/CalculatorBrain.swift
@@ -0,0 +1,25 @@
+//
+//  CalculatorBrain.swift
+//  swift3Calculator
+//
+//  Created by Jiajun Wu on 08/31/2017.
+//  Copyright © 2017 Jiajun Wu. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain{
+    func setOperand(_ operand: Double){
+        
+    }
+    
+    func preformOperation(_ symbol: String){
+        
+    }
+    
+    var result:Double{
+        get{
+            return 0
+        }
+    }
+}
--- swift3Calculator/ViewController.swift
+++ swift3Calculator/ViewController.swift
@@ -9,17 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    private var calculatorBrain = CalculatorBrain()
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var displayValue: Double{
+        get{
+            return Double(display.text!)!
+        }
+        set{
+            display.text = String(newValue)
+        }
+    }
+    
+    var isTypingDigit = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let displayText = display.text!
+        
+        let touchedDigit = sender.currentTitle!
+        
+        if isTypingDigit{
+            display.text = displayText + touchedDigit
+        }else{
+            display.text = touchedDigit
+        }
+        
+        isTypingDigit = true
+    }
+    
+    @IBAction func preformOperation(_ sender: UIButton) {
+        isTypingDigit = false
+        
+        let touchedOperation = sender.currentTitle!
+        
+        switch touchedOperation {
+        case "C":
+            display.text = "0"
+        default:
+            break
+        }
+    }
 }
 
